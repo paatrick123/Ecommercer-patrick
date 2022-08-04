6aee8112e20bba42681d73e5bd063a802b727b72
paatrick123 <patrickandrw@gmail.com>
2022-08-03 22:10:49 -0400

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,28 @@
 /*===== MENU SHOW =====*/ 
 const showMenu = (toggleId, navId) =>{
-    const togle = document.getElementById(toggleId),
+    const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
 
-    if(togle && nav){
-        togle.addEventListener('click',()=>{
-            navId.classList.toggle('show')
+    if(toggle && nav){
+        toggle.addEventListener('click',()=>{
+            nav.classList.toggle('show')
         })
     }
 }
 
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu');
+
 /*===== REMOVE MENU MOBILE =====*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction(){
-    //activo
+    //Activo
     navLink.forEach(n => n.classList.remove('active'));
     this.classList.add('active');
 
-    //remover el menu
+    // Remover el menu
     const navMenu = document.getElementById('nav-menu');
     navMenu.classList.remove('show');
 }
 
-navLink.forEach(n=> n.addEventListener('click', linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction))
